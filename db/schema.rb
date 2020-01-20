6a15015dfb14140de45d826b09287558cc4abc49
panatakashi <youho.capa@gmail.com>
2020-01-20 20:40:30 +0900

    done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20200104135807) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
